02274739f1a4d2b82b89ec55081b89ca0e438b84
Oded Welgreen <oded_wel@hotmail.com>
2014-05-31 15:45:34 +0300

    clean up pcg a bit

--- scripts/pcg.js
+++ scripts/pcg.js
@@ -1,21 +1,21 @@
-(function() {
-    var c = document.getElementById('pcg-canvas'),
-        context = c.getContext("2d"),ticker,
+(function () {
+    var c = document.getElementById('pcg-canvas'), $c = $(c),
+        context = c.getContext("2d"), ticker,
         centerx = context.canvas.width / 2, centery = context.canvas.height / 2, circ = Math.PI * 2,
         running = true, timeoutId,
-        fps = 60, numberOfSegmentsPerCircle = 30, circleCenters = [], mainCircleRadius = 60, smallCircleRadius = 30,
-        animateCircle = function(x, y, r, finishedCallback){
+        fps = 60, numberOfSegmentsPerCircle = 15, circleCenters = [], mainCircleRadius = 60, smallCircleRadius = 30, numberOfCircles = 30,
+        animateCircle = function (x, y, r, finishedCallback) {
             context.beginPath();
             var current = 0;
-            ticker.start(fps,function(){
-                if (!running)
+            ticker.start(fps, function () {
+                if (!running) {
                     return;
-                if (current < numberOfSegmentsPerCircle){
-                    context.arc(x, y, r, circ * (current / numberOfSegmentsPerCircle), circ * ((current+1) / numberOfSegmentsPerCircle), false);
+                }
+                if (current < numberOfSegmentsPerCircle) {
+                    context.arc(x, y, r, circ * (current / numberOfSegmentsPerCircle), circ * ((current + 1) / numberOfSegmentsPerCircle), false);
                     context.stroke();
                     current++;
-                }
-                else{
+                } else {
                     ticker.stop();   
                     finishedCallback();
                 }
@@ -24,17 +24,20 @@
 
     context.lineWidth = 2;
     context.strokeStyle = "#ccc";
-    function start(){
-        context.clearRect(0,0,200,200);
-        setTimeout(function(){
-            if (ticker) ticker.stop();
-            else ticker = new RAFTicker();   
+    function start() {
+        context.clearRect(0, 0, 200, 200);
+        setTimeout(function () {
+            if (ticker) {
+                ticker.stop();
+            } else {
+                ticker = new RAFTicker();   
+            }
             clearTimeout(timeoutId);
-            for (var i = 0;i<numberOfSegmentsPerCircle;i++) {
+            for (var i = 0; i<numberOfCircles; i++) {
                 circleCenters.push({ 
-                    x: centerx + mainCircleRadius * Math.cos(circ * (i / numberOfSegmentsPerCircle)),
-                    y: centery + mainCircleRadius * Math.sin(circ * (i / numberOfSegmentsPerCircle)),
-                    a: (circ * (i / numberOfSegmentsPerCircle)) * 180 / Math.PI
+                    x: centerx + mainCircleRadius * Math.cos(circ * (i / numberOfCircles)),
+                    y: centery + mainCircleRadius * Math.sin(circ * (i / numberOfCircles)),
+                    a: (circ * (i / numberOfCircles)) * 180 / Math.PI
                 });
             }
             context.clearRect(0,0,200,200);
@@ -43,13 +46,13 @@
                     var index = Math.floor((Math.random() * circleCenters.length));
                     var circle = circleCenters[index];
                     circleCenters.splice(index,1);
-                    $('#pcg-canvas').css('-webkit-transform','rotate('+(270 - circle.a)+'deg)');
-                    $('#pcg-canvas').css('transform','rotate('+(270 - circle.a)+'deg)');
-                    $('#pcg-canvas').css('-ms-transform','rotate('+(270 - circle.a)+'deg)');
+                    $c.css('-webkit-transform','rotate('+(270 - circle.a)+'deg)');
+                    $c.css('transform','rotate('+(270 - circle.a)+'deg)');
+                    $c.css('-ms-transform','rotate('+(270 - circle.a)+'deg)');
                     console.log(circle.a);
                     timeoutId = setTimeout(function(){
                         animateCircle(circle.x,circle.y,smallCircleRadius,chooseAndPaintCircle);
-                    },500);
+                    },900);
                 }
                 else{
                     start();   
@@ -58,12 +61,13 @@
             chooseAndPaintCircle();
         },500); 
     }
-
-    $('#pcg-canvas').click(function(){
-        running = !running;
+    
+    $c.click(function(){
+        running = !running;        
     });
 
     $(function(){
         start();
     });
+    
 }());
