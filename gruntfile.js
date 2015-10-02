256d7f6f99c28d77ff11b5b40f807742fe30cd46
Oded Welgreen <oded_wel@hotmail.com>
2015-10-02 22:08:46 +0300

    Add less compiling css bundling in gulp

--- gruntfile.js
+++ gruntfile.js
@@ -1,39 +1,39 @@
 module.exports = function(grunt) {
-    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks); 
+    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        cssmin: {  
-            sitecss: {  
-                options: {  
-                    banner: '/* My minified css file */'  
-                },  
-                files: {  
-                    'public/css/styles.min.css': [  
-                        'public/css/poole.css',  
-                        'public/css/lanyon.css',  
-                        'public/css/syntax.css',  
+        cssmin: {
+            sitecss: {
+                options: {
+                    banner: '/* My minified css file */'
+                },
+                files: {
+                    'public/css/styles.min.css': [
+                        'public/css/poole.css',
+                        'public/css/lanyon.css',
+                        'public/css/syntax.css',
                         'public/css/site.css'
                     ]
-                }  
-            }  
-        },  
+                }
+            }
+        },
         uglify: {
-            options: {  
-                compress: true  
-            },  
-            applib: {  
-                src: [  
-                    'scripts/circle.js',  
-                    'scripts/pcg.js',  
-                ],  
-                dest: 'scripts/applib.js'  
-            } 
+            options: {
+                compress: true
+            },
+            applib: {
+                src: [
+                    'scripts/circle.js',
+                    'scripts/pcg.js',
+                ],
+                dest: 'scripts/applib.js'
+            }
         }
     });
 
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify', 'cssmin']);  
+    grunt.registerTask('default', ['uglify', 'cssmin']);
 
 };
---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
+var watch = require('gulp-watch');
+var concat = require('gulp-concat');
+var minifyCss = require('gulp-minify-css');
+
+gulp.task('less', function () {
+  return gulp.src('styles/less/*.less')
+  .pipe(less())
+  .pipe(gulp.dest('styles/css'));
+});
+
+gulp.task('bundle-css', ['less'], function() {
+ return gulp.src([
+ 'styles/css/poole.css',
+ 'styles/css/lanyon.css',
+ 'styles/css/syntax.css',
+ 'styles/css/site.css'
+])
+   .pipe(concat('styles.min.css'))
+   .pipe(minifyCss())
+   .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('watch', function () {
+   gulp.watch('styles/less/*.less', ['bundle-css']);
+});
+
+gulp.task('default', ['less', 'watch']);
