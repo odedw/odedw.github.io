607663c536ab5bd136625d0dc1a999bc30de86d3
Oded Welgreen <oded.welgreen@gmail.com>
2018-07-29 21:16:45 +0300

    add codepens

---
+++ src/pages/projects.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import Link from 'gatsby-link';
+import styled from 'styled-components';
+import { graphql } from 'graphql';
+import importScripts from '../utils/importScripts';
+
+const Container = styled.div`padding 1em 0;`;
+const CodepenContainer = styled.li`list-style: none;`;
+class ProjectsPage extends React.Component {
+    componentDidMount() {
+        importScripts([ 'https://codepen.io/assets/embed/ei.js' ]); //.then(() => {});
+    }
+    render() {
+        const { data } = this.props;
+        return (
+            <Container>
+                <ul>
+                    {data.allCodepensJson.edges.map((obj) => obj.node).map((codepen) => (
+                        <CodepenContainer>
+                            <h3>{codepen.name}</h3>
+                            <p
+                                data-height="329"
+                                data-theme-id="6466"
+                                data-slug-hash={codepen.hash}
+                                data-default-tab="result"
+                                className="codepen"
+                            />
+                        </CodepenContainer>
+                    ))}
+                </ul>
+            </Container>
+        );
+        return;
+    }
+}
+export default ProjectsPage;
+export const query = graphql`
+    query SiteProjectsQuery {
+        allProjectsJson {
+            edges {
+                node {
+                    name
+                    gfycat
+                    shortDesc
+                    repo
+                    link
+                }
+            }
+        }
+        allCodepensJson {
+            edges {
+                node {
+                    hash
+                    name
+                }
+            }
+        }
+    }
+`;
